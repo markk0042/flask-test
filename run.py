919cda058c139c0aeee69734122d36b91ac34079
Mark Kelly <markk0042@gmail.com>
2020-07-03 14:17:27 +0000

    updated for loop on about.html and also run.py file company defined and aldo data and images added to the about us section for multiple people with images

--- run.py
+++ run.py
@@ -1,19 +1,27 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/about')
 def about():
-    return render_template("about.html", page_title="About", list_of_numbers=[1,2,3])
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company =data)
+
 
 @app.route('/contact')
 def contact():
     return render_template("contact.html", page_title="Contact")
+
 
 @app.route('/careers')
 def careers():
